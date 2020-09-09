1670c3bd0b4bf51d2814d8ab38afa8748df90d41
Nick Park <niko_park@mail.ru>
2020-09-09 20:11:57 +0700

    cookie...om-nom-nom

--- chapter_3/tmp.cc
+++ chapter_3/tmp.cc
@@ -4,16 +4,25 @@ using namespace std;
 
 int main() 
 {
-    string s("some string");
-    if (s.begin() != s.end()) {
-        auto it = s.begin();
-        *it = toupper(*it);
-    }
-    cout << s << endl;
-    string p("some string");
-    for (auto it2 = p.begin(); it2 != p.end() && !isspace(*it2); ++it2)
-        *it2 = toupper(*it2);
-    cout << p << endl;
+    //string s("some string");
+    //if (s.begin() != s.end()) {
+        //auto it = s.begin();
+        //*it = toupper(*it);
+    //}
+    //cout << s << endl;
+    //string p("some string");
+    //for (auto it2 = p.begin(); it2 != p.end() && !isspace(*it2); ++it2)
+        //*it2 = toupper(*it2);
+    //cout << p << endl;
+    unsigned scores[11] = {};
+    unsigned grade;
+    while (cin >> grade) {
+      if (grade <= 100)
+        ++scores[grade/10];
+      }
+    
+    for (auto &i : scores)
+      cout << i << " ";
     return 0;
 }
 
